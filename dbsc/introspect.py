a3f4b038392191ebf03bc3ab62f2366a537f52b4
J Kishore kumar <justjkk@gmail.com>
2013-07-25 19:13:49 +0530

    Added requirements.txt; Added __name__ check for Introspect

--- dbsc/introspect.py
+++ dbsc/introspect.py
@@ -7,25 +7,26 @@ from dbsc.settings import CONNECTION_STRING
 engine = create_engine(CONNECTION_STRING)
 insp = reflection.Inspector.from_engine(engine)
 
-for table_name in insp.get_table_names():
-    print('[' + table_name + '] {color:blue}')
-    primary_keys = insp.get_primary_keys(table_name)
-    foreign_keys = insp.get_foreign_keys(table_name)
-    for column in insp.get_columns(table_name):
-        print(' ', end='')
-        if column['name'] in primary_keys:
-            print('*', end='')
-        else:
+if __name__ == "__main__":
+    for table_name in insp.get_table_names():
+        print('[' + table_name + '] {color:blue}')
+        primary_keys = insp.get_primary_keys(table_name)
+        foreign_keys = insp.get_foreign_keys(table_name)
+        for column in insp.get_columns(table_name):
             print(' ', end='')
-        print(column['name'], end='')
-        if any(column['name'] in fk['constrained_columns']
-               for fk in foreign_keys):
-            print('*')
-        else:
-            print('')
-    print('')
+            if column['name'] in primary_keys:
+                print('*', end='')
+            else:
+                print(' ', end='')
+            print(column['name'], end='')
+            if any(column['name'] in fk['constrained_columns']
+                   for fk in foreign_keys):
+                print('*')
+            else:
+                print('')
+        print('')
 
-for table_name in insp.get_table_names():
-    for foreign_key in insp.get_foreign_keys(table_name):
-        print('[' + foreign_key['referred_table'] + '] 1--* [' + table_name +
-              ']')
+    for table_name in insp.get_table_names():
+        for foreign_key in insp.get_foreign_keys(table_name):
+            print('[' + foreign_key['referred_table'] + '] 1--* [' + table_name +
+                  ']')
